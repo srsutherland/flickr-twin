d1c138e5265843a82e8438f03094cc45c7fca5b7
Sean Sutherland <SeanRSuther@gmail.com>
2020-10-15 17:34:19 -0700

    downloadObjectAsJson -> util.js

--- flickr-twin.js
+++ flickr-twin.js
@@ -8,13 +8,4 @@
 
 /* file source/Controller.js */
 
-// eslint-disable-next-line no-unused-vars
-function downloadObjectAsJson(exportObj, exportName) {
-  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
-  var downloadAnchorNode = document.createElement('a');
-  downloadAnchorNode.setAttribute("href", dataStr);
-  downloadAnchorNode.setAttribute("download", exportName + ".json");
-  document.body.appendChild(downloadAnchorNode); // required for firefox
-  downloadAnchorNode.click();
-  downloadAnchorNode.remove();
-}
+
---
+++ source/util.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+function downloadObjectAsJson(exportObj, exportName) {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+}
